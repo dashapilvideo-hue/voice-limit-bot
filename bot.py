a372aeb360522e5adff541bbe6f8a8ff95156483
dashapilvideo-hue <dashapilvideo@gmail.com>
2026-02-17 19:48:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
-import telebot
 import os
+import telebot
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.environ.get("TOKEN")
+
+if not TOKEN:
+    raise ValueError("TOKEN not found!")
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
-    if message.voice.duration > 15:
-        bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(
-            message.chat.id,
-            f"{message.from_user.first_name}, голосовые сообщения должны быть не длиннее 15 секунд!"
-        )
+    try:
+        if message.voice.duration > 15:
+            bot.delete_message(message.chat.id, message.message_id)
+            bot.send_message(
+                message.chat.id,
+                f"{message.from_user.first_name}, голосовые сообщения должны быть не длиннее 15 секунд!"
+            )
+    except Exception as e:
+        print(f"Error: {e}")
 
-bot.polling()
+print("Bot started...")
+bot.infinity_polling()
